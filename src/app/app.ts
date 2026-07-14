4ca6113fb5e5573502dbcdc13c75496e20dfe932
liliani mendes <mendessantos0023@gmail.com>
2026-07-14 19:12:50 -0300

    style (produtos): criar componente ProdutoListaComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+//import { RouterOutlet } from '@angular/router';//renove a importação do RouteOutlet, pois não é necessário para este componente
+import {Produto} from './components/produto/produto';//importando a classe produto do arquivo produto,ts para ser usado no componente app
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
