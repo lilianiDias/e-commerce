d9184364c7de6bade61a9e9dd7a1db3ac75587b5
liliani mendes <mendessantos0023@gmail.com>
2026-07-15 19:39:16 -0300

    feat(app): adiciona lis-produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';//renove a importação do RouteOutlet, pois não é necessário para este componente
-import {Produto} from './components/produto/produto';//importando a classe produto do arquivo produto,ts para ser usado no componente app
+//import {Produto} from './components/produto/produto';//importando a classe produto do arquivo produto,ts para ser usado no componente ap
+import { LisProdutos } from './components/lis-produtos/lis-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [LisProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
