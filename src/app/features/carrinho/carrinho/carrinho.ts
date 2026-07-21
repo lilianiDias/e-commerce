1c674e76496708ee2a4c6dbd6439f3417b042a91
liliani mendes <betoneirasegames@gmail.com>
2026-07-21 19:55:29 -0300

    feat(app.html): adiciona cabeçalho com nome da loja e links de navegação

---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {
+//! Lógica de carrinho virá na proxímas aulas 
+//! por ora, apenas exibir a página carrinho
+}
