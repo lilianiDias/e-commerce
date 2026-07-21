9aec8c4d5c288dbb03ca3139f5deea64d95e8e22
liliani mendes <betoneirasegames@gmail.com>
2026-07-21 19:59:40 -0300

    feat(routes): adiciona rota para o componente de carrinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LisProdutos } from './features/produtos/lis-produtos/lis-produtos';
+import { Carrinho } from './features/carrinho/carrinho/carrinho';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: '',
+    component: LisProdutos,
+},
+{
+    path: 'carrinho',
+    component: Carrinho,
+},
+];
