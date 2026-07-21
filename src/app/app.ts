f8bc8f80733cb0863b8a669b18a6d0e3ab0c625d
liliani mendes <betoneirasegames@gmail.com>
2026-07-21 20:03:06 -0300

    refactor(app): atualiza imports e propriedades do componente raiz

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';//renove a importação do RouteOutlet, pois não é necessário para este componente
-//import {Produto} from './components/produto/produto';//importando a classe produto do arquivo produto,ts para ser usado no componente ap
-import { LisProdutos } from './features/produtos/lis-produtos/lis-produtos';
+import { RouterOutlet, RouterLink} from '@angular/router';//!renove a importação do RouteOutlet, pois não é necessário para este componente
+import { UpperCasePipe } from '@angular/common';
+//!import {Produto} from './components/produto/produto';//!importando a classe produto do arquivo produto,ts para ser usado no componente ap
+
 @Component({
   selector: 'app-root',
-  imports: [LisProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Loja sol';
 }
