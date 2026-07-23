c5fabb38dd4bb8ecf6879fd5294252ff1dfc9f07
liliane mendes <mendessantos0023@gmail>
2026-07-23 19:04:15 -0300

    feat(app): adicionar links de navegação e controles de status de login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink} from '@angular/router';//!renove a importação do RouteOutlet, pois não é necessário para este componente
 import { UpperCasePipe } from '@angular/common';
+import { LowerCasePipe } from '@angular/common';
+import { usuarioLogado, login, logout } from './core/auth';
 //!import {Produto} from './components/produto/produto';//!importando a classe produto do arquivo produto,ts para ser usado no componente ap
 
 @Component({
@@ -11,5 +13,8 @@ import { UpperCasePipe } from '@angular/common';
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Loja sol';
+  nomeLoja = 'sol';
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
