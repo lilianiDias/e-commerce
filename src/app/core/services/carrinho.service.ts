503494bd9cc6136e6e2b6a8ef339e44fdd622b38
liliani mendes <mendessantos00232gmail.com>
2026-08-12 22:03:12 -0300

    refactor: finaliza a implementacao do CarrinhoService com signals

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,5 +1,10 @@
 import { Injectable,signal,computed } from '@angular/core';
 
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+};  
+
 @Injectable({ 
     providedIn: 'root'
 })
@@ -7,13 +12,13 @@ import { Injectable,signal,computed } from '@angular/core';
 export class CarrinhoService {
 
     //! Estado GlobalCriado com sussseso
-    private carrinho = signal<{nome: string; preco: number}[]>([]);
+    private carrinho = signal<ItemCarrinho[]>([]);
 
 //? Seleção 
  itens = computed(() => this.carrinho());
  quantidadeItens = computed(() => this.carrinho().length);
- totalItens= computed(() => 
-this.carrinho().reduce((total, item) => total + item.preco, 0));
+ totalItens = computed(() => this.carrinho().reduce((total, item) => total + item.preco, 0));
+carrinhoVazio = computed(() => this.carrinho().length === 0);
 
 // TODO: ação Adicionar produtos
 adicionar(produto: {nome: string; preco: number}){
