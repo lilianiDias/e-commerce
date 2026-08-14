3d3cf7b5a5980dd46817b6d1bc1ba61737b9a56a
liliani mendes <mendessantos0023@gmail.com>
2026-08-14 19:31:23 -0300

    Refatorar AppComponent: limpar imports não utilizados e organizar metadados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink} from '@angular/router';//!renove a importação do RouteOutlet, pois não é necessário para este componente
 import { UpperCasePipe } from '@angular/common';
-import { LowerCasePipe } from '@angular/common';
-import { usuarioLogado, login, logout } from './core/auth';
 import { MatButtonModule } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 import { Header } from './shared/layout/header/header'; 
 //!import {Produto} from './components/produto/produto';//!importando a classe produto do arquivo produto,ts para ser usado no componente ap
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, MatCardModule, Header],
+  imports: [RouterOutlet,  MatButtonModule,  Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'sol';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
 }
