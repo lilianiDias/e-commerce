579490b134667c8045ddad68d21a971049858259
liliani mendes <mendessantos0023@gmail.com>
2026-08-14 19:29:26 -0300

    feat(routes)Configurar rotas do Angular com lazy loading e guarda de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,8 +24,15 @@ export const routes: Routes = [
     },
     {
      path:'checkout',
+      canActivate: [authGuard],
      loadComponent: () =>
         import('./features/checkout/checkout/checkout').then((m) => m.Checkout),
+    },
+
+    {
+     path:'login',
+     loadComponent: () =>
+        import('./features/login/login/login').then((m) => m.Login),
     },
     {
         
