251310037f4de4641580c245e72b2fab59398797
liliani mendes <mendessantos0023@gmail.com>
2026-08-14 21:32:17 -0300

    feat(header): conectar authService e implementar acao de logout na toolbar

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import {CarrinhoService} from '../../../core/services/carrinho.service';
 import { inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -15,4 +16,15 @@ export class Header {
   nomeLoja= 'Sol';
   private carrinhoService = inject(CarrinhoService);
   quantidadeHeader= this.carrinhoService.quantidadeItens;
+
+  private authService = inject(AuthService);
+  usuarioLogado = this.authService.usuarioLogado;
+  usuarioAtual = this.authService.usuarioAtual;
+
+  private router = inject(Router);
+
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
