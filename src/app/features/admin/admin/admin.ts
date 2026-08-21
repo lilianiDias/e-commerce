6703f362305ab46e51f094e682d7eba76714532a
Matheus Dias <mendesantos0023@gmail.com>
2026-08-20 21:51:55 -0300

    feat(admin): criar componente da página de administração com integração ao AuthFacade

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,7 +1,6 @@
 import { Component, computed, signal, inject } from '@angular/core';
 import { Router} from '@angular/router';
-import { AuthService } from '../../../core/services/auth.service';
-
+import { AuthFacade } from '../../../core/facades/auth.facades';
 @Component({
   selector: 'app-admin',
   imports: [],
@@ -11,14 +10,14 @@ import { AuthService } from '../../../core/services/auth.service';
 export class Admin {
 
   private router = inject(Router);
-  private  authService = inject(AuthService);
+  private  authFacade = inject(AuthFacade);
 
 
   totalProdutosCadastrado = signal(20);
   pedidosPendentes = signal(6);
   usuarioCadastrados = signal(7);
 
-  usuarioAtual = this.authService.usuarioAtual
+  usuarioAtual = this.authFacade.usuarioAtual
 
   areaPerfil = computed(() =>{
 const usuario = this.usuarioAtual();
@@ -29,7 +28,7 @@ if(!usuario){
 return 'Usuário autenticado como: ${usuario.perfil}';
   });
   sair(){
-    this.authService.logout();
+    this.authFacade.sair();
     this.router.navigateByUrl('/login');
   }
 }
